ff3324d222eed35159041e5a7118aa5f745e4f28
Yocode5 <155065982+Yocode5@users.noreply.github.com>
2026-08-22 14:47:07 +0530

    feat: enhance task management UI with new layout and styles

--- src/components/ErrorAlert/ErrorAlert.jsx
+++ src/components/ErrorAlert/ErrorAlert.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function ErrorAlert({ message, onRetry }) {
   if (!message) return null;
 
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,7 +1,6 @@
 import Header from "../components/Header/Header";
 import WelcomeSection from "../components/WelcomeSection/WelcomeSection";
 import TaskGrid from "../components/Tasks/TaskGrid";
-import TaskCard from "../components/Tasks/TaskCard";
 
 export default function TasksPage() {
     return (
