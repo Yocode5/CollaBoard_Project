c8fd27de32e13ea0f6d2bd2e804d220522c04afe
Samadhi Chandrasekara <casuchandrasekara@students.nsbm.ac.lk>
2026-09-13 19:59:56 +0530

    fix

--- client/src/components/Tasks/TaskGrid.test.jsx
+++ client/src/components/Tasks/TaskGrid.test.jsx
@@ -1,3 +1,5 @@
+/* eslint-env vitest */
+
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import TaskGrid from './TaskGrid';
